617b8824a4cfe9d2c3a7daac07f03f00ebe92340
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2025-03-03 11:58:36 +0000

    Feature - Made the search box responsive

--- scripts/searchBtn.js
+++ scripts/searchBtn.js
@@ -5,10 +5,16 @@ const form = document.querySelector('form');
 searchBtn.addEventListener('click', () => {
 	form.classList.add('active');
 	searchBtn.classList.add('active');
+
+	if (window.innerWidth <= 991) {
+		document.body.style.overflow = 'hidden';
+	}
 });
 
 searchCloseBtn.addEventListener('click', e => {
 	e.preventDefault();
 	form.classList.remove('active');
 	searchBtn.classList.remove('active');
+
+	document.body.style.overflow = 'auto';
 });
